06fe2a5df1d454227198d256944918f73e8957fa
Elina Khasanova <e.khasanova@setpartnerstv.ru>
2022-01-29 13:43:09 +0300

    test2

--- src/components/hello-world/HelloWorld.vue
+++ src/components/hello-world/HelloWorld.vue
@@ -35,20 +35,4 @@ const count = ref(0);
     </p>
 </template>
 
-<style scoped lang="scss">
-a {
-    color: #42b983;
-}
-
-label {
-    margin: 0 0.5em;
-    font-weight: bold;
-}
-
-code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
-}
-</style>
+<style scoped lang="scss" src="./HelloWorld.scss"></style>
